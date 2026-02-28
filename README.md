8648ca98593f5a3a240adcfcc10e78b079beb1b4
chantrix <msrapallo@gmail.com>
2026-02-28 12:34:54 +0100

    añado script y README actualizado

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # asir_proyecto_2026
 el proyecto final de ASIR
+
+Este proyecto despliega una nube privada Nextcloud con autenticacion
+centralizada mediante openLDAP y proxy inverso INGNX todo mediante 
+Docker.
+
+Servicios:
++NEXTCLOUD: servicio de almacenamiento
++OPENLDAP PHPLDAPADMIN: gestion de usuarios y contraseñas
++NGINX: proxy inverso en puerto 4443, gestion de ssl
++MARIADB: servicio de base de datos para Nextcloud
+
+Requisitos:
++Tener instalado Docker
++Tener un subdominio de duckdns.org
++Abrir los puertos del router 80 y 4443 y apuntando al servidor
+
+Despliegue:
+1+Crear las variables de entorno a partir del archivo .env.demo 
+	y guardarlas como .env
+2+Levantar la infraestructura con:
+		docker compose up -d
+3+Debido a el uso del puerto 4443 debemos ejecutar el script:
+	./scriptInicio.sh
---
+++ scriptInicio.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#  Carga las variables del archivo .env
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+    echo " Archivo .env cargado correctamente."
+else
+    echo " Error: No se encuentra el archivo .env"
+    exit 1
+fi
+
+#  Define el dominio (usando la variable de tu .env + el puerto)
+DOMINIO_COMPLETO="${MI_DOMINIO}:4443"
+
+echo " Configurando Nextcloud para: $DOMINIO_COMPLETO"
+
+# Ejecuta comandos de configuración en el contenedor
+docker exec --user www-data nextcloud-asir php occ config:system:set trusted_domains 1 --value=$DOMINIO_COMPLETO
+docker exec --user www-data nextcloud-asir php occ config:system:set overwritehost --value=$DOMINIO_COMPLETO
+docker exec --user www-data nextcloud-asir php occ config:system:set overwriteprotocol --value=https
+# Limpia caché de rutas 
+docker exec --user www-data nextcloud-asir php occ maintenance:update:htaccess
+
+echo "Configuración finalizada con exito"
