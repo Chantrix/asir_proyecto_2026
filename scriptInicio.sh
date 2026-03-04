fa609b90a99f2220b795a21d7ccf6fa7b6271184
chantrix <msrapallo@gmail.com>
2026-03-04 15:59:32 +0100

    cambios en compose.yaml para que funcione en windows-- los volumes ya no son bind

--- scriptInicio.sh
+++ scriptInicio.sh
@@ -15,8 +15,8 @@ DOMINIO_COMPLETO="${MI_DOMINIO}:4443"
 echo " Configurando Nextcloud para: $DOMINIO_COMPLETO"
 
 # Ejecuta comandos de configuración en el contenedor
-docker exec --user www-data nextcloud-asir php occ config:system:set trusted_domains 1 --value=$DOMINIO_COMPLETO
-docker exec --user www-data nextcloud-asir php occ config:system:set overwritehost --value=$DOMINIO_COMPLETO
+docker exec --user www-data nextcloud-asir php occ config:system:set trusted_domains 1 --value=cloud.$DOMINIO_COMPLETO
+docker exec --user www-data nextcloud-asir php occ config:system:set overwritehost --value=cloud.$DOMINIO_COMPLETO
 docker exec --user www-data nextcloud-asir php occ config:system:set overwriteprotocol --value=https
 # Limpia caché de rutas 
 docker exec --user www-data nextcloud-asir php occ maintenance:update:htaccess
